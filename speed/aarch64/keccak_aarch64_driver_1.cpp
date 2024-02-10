237032e3c1a499501fd4164936f347ffdf6603fc
Kris Kwiatkowski <contact@amongbytes.com>
2024-02-10 16:26:06 +0000

    sha3_2

--- speed/aarch64/keccak_aarch64_driver_1.cpp
+++ speed/aarch64/keccak_aarch64_driver_1.cpp
@@ -1,34 +1,51 @@
 #include <benchmark/benchmark.h>
 #include <gbench/src/cycleclock.h>
 #include <gbench/src/statistics.h>
-
 #include <stdint.h>
 
 extern "C" {
-	void keccakf1600_asm_1(uint64_t *);
+void keccakf1600_asm_1(uint64_t *);
+void keccakf1600_asm_2(uint64_t *);
 }
 
 // Utility for reporting cycleclocks.
-static inline void add_cycleclock(benchmark::State& st, int64_t cycles) {
-        st.counters["Cycles"] =
-            benchmark::Counter(cycles,
-                benchmark::Counter::kAvgIterations | benchmark::Counter::kResultNoFormat);
+static inline void add_cycleclock(benchmark::State &st, int64_t cycles) {
+    st.counters["Cycles"] =
+        benchmark::Counter(cycles, benchmark::Counter::kAvgIterations |
+                                       benchmark::Counter::kResultNoFormat);
 }
 
 static void keccakf1600_asm_1(benchmark::State &st) {
-	size_t t, total = 0;
+    size_t t, total = 0;
     uint64_t state[25];
     for (auto _: st) {
-    	t = benchmark::cycleclock::Now();
+        t = benchmark::cycleclock::Now();
 
-    	keccakf1600_asm_1(state);
+        keccakf1600_asm_1(state);
 
-    	total += benchmark::cycleclock::Now() - t;
-    	benchmark::DoNotOptimize(t);
-    	benchmark::DoNotOptimize(total);
-    	benchmark::ClobberMemory();
+        total += benchmark::cycleclock::Now() - t;
+        benchmark::DoNotOptimize(t);
+        benchmark::DoNotOptimize(total);
+        benchmark::ClobberMemory();
+    }
+    add_cycleclock(st, total);
+}
+
+static void keccakf1600_asm_2(benchmark::State &st) {
+    size_t t, total = 0;
+    uint64_t state[25];
+    for (auto _: st) {
+        t = benchmark::cycleclock::Now();
+
+        keccakf1600_asm_2(state);
+
+        total += benchmark::cycleclock::Now() - t;
+        benchmark::DoNotOptimize(t);
+        benchmark::DoNotOptimize(total);
+        benchmark::ClobberMemory();
     }
     add_cycleclock(st, total);
 }
 
 BENCHMARK(keccakf1600_asm_1)->Unit(benchmark::kNanosecond);
+BENCHMARK(keccakf1600_asm_2)->Unit(benchmark::kNanosecond);
