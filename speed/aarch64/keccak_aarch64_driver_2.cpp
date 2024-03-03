1ac6777fb918a8b30de2eb7f70c94b0ba79321e9
Kris Kwiatkowski <contact@amongbytes.com>
2024-03-03 21:31:24 +0000

    [gbench] Update gbench version

--- speed/aarch64/keccak_aarch64_driver_2.cpp
+++ speed/aarch64/keccak_aarch64_driver_2.cpp
@@ -1,11 +1,12 @@
+#include <assert.h>
 #include <benchmark/benchmark.h>
 #include <gbench/src/cycleclock.h>
 #include <gbench/src/statistics.h>
 #include <stdint.h>
+#include <string.h>
 
 extern "C" {
-void keccakf1600_asm_1(uint64_t *);
-void keccakf1600_asm_2(uint64_t *);
+void keccakf1600x2_asm_1(uint64_t *);
 }
 
 // Utility for reporting cycleclocks.
@@ -17,37 +18,24 @@ static inline void add_cycleclock(benchmark::State &st, int64_t cycles) {
     }
 }
 
-static void keccakf1600_asm_1(benchmark::State &st) {
+static void keccakf1600x2(benchmark::State &st) {
     size_t t, total = 0;
-    uint64_t state[25];
+    uint64_t state[25 * 2], s2[25 * 2];
+    for (size_t i = 0; i < 25; i++) { state[i] = 1; }
+    for (size_t i = 25; i < 50; i++) { state[i] = 2; }
+    memcpy(s2, state, sizeof(uint64_t) * 50);
     for (auto _: st) {
         t = benchmark::cycleclock::Now();
 
-        keccakf1600_asm_1(state);
+        keccakf1600x2_asm_1(state);
 
         total += benchmark::cycleclock::Now() - t;
         benchmark::DoNotOptimize(t);
         benchmark::DoNotOptimize(total);
         benchmark::ClobberMemory();
     }
+    //for (size_t i = 0; i < 50; i++) { assert(state[i] == s2[i]); }
     add_cycleclock(st, total);
 }
 
-static void keccakf1600_asm_2(benchmark::State &st) {
-    size_t t, total = 0;
-    uint64_t state[25];
-    for (auto _: st) {
-        t = benchmark::cycleclock::Now();
-
-        keccakf1600_asm_2(state);
-
-        total += benchmark::cycleclock::Now() - t;
-        benchmark::DoNotOptimize(t);
-        benchmark::DoNotOptimize(total);
-        benchmark::ClobberMemory();
-    }
-    add_cycleclock(st, total);
-}
-
-BENCHMARK(keccakf1600_asm_1)->Unit(benchmark::kNanosecond);
-BENCHMARK(keccakf1600_asm_2)->Unit(benchmark::kNanosecond);
+BENCHMARK(keccakf1600x2)->Unit(benchmark::kNanosecond);
